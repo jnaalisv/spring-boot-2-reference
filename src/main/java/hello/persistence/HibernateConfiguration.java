ce2f1a0c72bd5518add7de9c09c55867f954f15a
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-04-13 18:27:13 +0300

    configure Hibernate through application properties

---
+++ src/main/java/hello/persistence/HibernateConfiguration.java
@@ -0,0 +1,36 @@
+package hello.persistence;
+
+import org.hibernate.SessionFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableConfigurationProperties
+public class HibernateConfiguration {
+
+    @Bean
+    @ConfigurationProperties("spring.jpa.properties")
+    public Properties hibernateProperties() {
+        return new Properties();
+    }
+
+    @Bean
+    public SessionFactory sessionFactory(DataSource dataSource, Properties hibernateProperties) {
+        return new LocalSessionFactoryBuilder(dataSource)
+                .scanPackages("hello.domain")
+                .addProperties(hibernateProperties)
+                .buildSessionFactory();
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        return new HibernateTransactionManager(sessionFactory);
+    }
+}
