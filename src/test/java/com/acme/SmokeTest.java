8c9c29f993593688732575886081e1bfdd12641d
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-08-08 12:42:59 +0300

    reduce visibility

--- src/test/java/com/acme/SmokeTest.java
+++ src/test/java/com/acme/SmokeTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.net.URI;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,13 +20,13 @@ import static org.assertj.core.api.Assertions.assertThat;
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = ExampleApplication.class
 )
-public class SmokeTest {
+class SmokeTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    public void applicationShouldRespond() {
+    void applicationShouldRespond() {
         assertThat(
                 restTemplate
                         .getForEntity("/products", String.class)
@@ -33,7 +35,7 @@ public class SmokeTest {
     }
 
     @Test
-    public void shouldBeAbleToAddAndListProducts() {
+    void shouldBeAbleToAddAndListProducts() {
         assertThat(
                 restTemplate
                 .getForEntity("/products", List.class)
