653946baa0f68df8cd960cf507410fac8b80f895
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-08-23 18:58:30 +0300

    replace star imports with explicit imports

--- src/main/java/com/acme/http/ProductController.java
+++ src/main/java/com/acme/http/ProductController.java
@@ -1,11 +1,15 @@
 package com.acme.http;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.acme.domain.Product;
 import com.acme.domain.ProductService;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
--- src/test/java/com/acme/http/ProductResourceTests.java
+++ src/test/java/com/acme/http/ProductResourceTests.java
@@ -10,7 +10,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
