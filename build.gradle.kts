0f0797a4b90c9ffc7bc10aa348f2657ae803535e
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-08-23 18:13:35 +0300

    Java11 compat, jacoco had to be disabled for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    jacoco
+    //jacoco
     id("io.spring.dependency-management") version "1.0.6.RELEASE"
     id("org.springframework.boot") version "2.1.0.M2"
 
@@ -9,6 +9,10 @@ plugins {
 
 version = "0.0.1-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
 repositories {
     mavenCentral()
     maven("https://repo.spring.io/milestone")
@@ -16,6 +20,18 @@ repositories {
 
 springBoot {
     buildInfo()
+}
+
+dependencyManagement {
+    dependencies {
+        // JAXB (JSR 222) Standalone Implementation
+        dependency ("javax.xml.bind:jaxb-api:2.3.0")
+        dependency ("com.sun.xml.bind:jaxb-impl:2.3.0")
+        dependency ("com.sun.xml.bind:jaxb-core:2.3.0")
+
+        // JSR-925 Java Beans Activation Framework
+        dependency ("javax.activation:activation:1.1.1")
+    }
 }
 
 dependencies {
@@ -27,6 +43,12 @@ dependencies {
     runtime("com.h2database:h2")
 
     compile("net.ttddyy:datasource-proxy:1.4.9")
+
+    runtime ("javax.xml.bind:jaxb-api")
+    runtime ("com.sun.xml.bind:jaxb-impl")
+    runtime ("com.sun.xml.bind:jaxb-core")
+    runtime ("javax.activation:activation")
+
     testCompile("org.springframework.boot:spring-boot-starter-test")
     testCompile("org.junit.jupiter:junit-jupiter-api")
 
@@ -36,11 +58,11 @@ dependencies {
 val test: Test by tasks
 test.useJUnitPlatform()
 
-val jacocoTestReport: JacocoReport by tasks
-jacocoTestReport.reports {
-    xml.isEnabled = false
-    html.isEnabled = true
-    html.destination = file("${buildDir}/jacocoHtml")
-}
+//val jacocoTestReport: JacocoReport by tasks
+//jacocoTestReport.reports {
+//    xml.isEnabled = false
+//    html.isEnabled = true
+//    html.destination = file("${buildDir}/jacocoHtml")
+//}
 
-test.finalizedBy(jacocoTestReport)
+//test.finalizedBy(jacocoTestReport)
