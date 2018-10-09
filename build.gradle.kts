0673de601973fd483f91931c8d750922f5339708
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-10-09 13:45:03 +0300

    bump Spring Boot to 2.1.0.M4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     jacoco
     id("io.spring.dependency-management") version "1.0.6.RELEASE"
-    id("org.springframework.boot") version "2.1.0.M2"
+    id("org.springframework.boot") version "2.1.0.M4"
 
     id("com.gorylenko.gradle-git-properties") version "1.4.21"
 }
