31f31ca3a92faa9e309374ed0bb9e3d1ea6d2980
Juho Naalisvaara <jnaalisv@hal.local>
2018-12-28 13:33:04 +0200

    simplifuy jacoco and test config

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,14 +41,17 @@ dependencies {
     testRuntime("org.junit.jupiter:junit-jupiter-engine")
 }
 
-val test: Test by tasks
-test.useJUnitPlatform()
+tasks {
+    jacocoTestReport {
+        reports {
+            xml.isEnabled = false
+            html.isEnabled = true
+            html.destination = file("${buildDir}/reports/tests/jacoco")
+        }
+    }
 
-val jacocoTestReport: JacocoReport by tasks
-jacocoTestReport.reports {
-    xml.isEnabled = false
-    html.isEnabled = true
-    html.destination = file("${buildDir}/jacocoHtml")
+    test {
+        useJUnitPlatform()
+        finalizedBy(jacocoTestReport)
+    }
 }
-
-test.finalizedBy(jacocoTestReport)
