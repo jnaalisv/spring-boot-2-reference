2fb338e0050ead9fcfd99ea9e694bcd2a9258d87
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2019-01-12 12:44:14 +0200

    Spring Boot 2.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     jacoco
     id("io.spring.dependency-management") version "1.0.6.RELEASE"
-    id("org.springframework.boot") version "2.1.1.RELEASE"
+    id("org.springframework.boot") version "2.1.2.RELEASE"
 
     id("com.gorylenko.gradle-git-properties") version "1.4.21"
 }
