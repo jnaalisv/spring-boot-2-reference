5ac8a148d9a661ca20d0f58a42c8df749ad76955
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2019-08-08 22:40:36 +0300

    bump deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     jacoco
     id("io.spring.dependency-management") version "1.0.6.RELEASE"
-    id("org.springframework.boot") version "2.1.6.RELEASE"
+    id("org.springframework.boot") version "2.1.7.RELEASE"
 
     id("com.gorylenko.gradle-git-properties") version "1.4.21"
 }
@@ -22,9 +22,9 @@ springBoot {
 }
 
 val dsProxyVersion = "1.5.1"
-ext["hibernate.version"] = "5.4.3.Final"
+ext["hibernate.version"] = "5.4.4.Final"
 ext["hikaricp.version"] = "3.3.1"
-ext["junit-jupiter.version"] = "5.4.2"
+ext["junit-jupiter.version"] = "5.5.1"
 
 dependencies {
     annotationProcessor ("org.immutables:value:2.7.4")
