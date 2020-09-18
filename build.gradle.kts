c9c4401fb162ec1d4ab125da2b8042102bb2bebd
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2020-09-18 22:57:31 +0300

    Spring Boot 2.3.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     jacoco
     id("io.spring.dependency-management") version "1.0.7.RELEASE"
-    id("org.springframework.boot") version "2.3.2.RELEASE"
+    id("org.springframework.boot") version "2.3.4.RELEASE"
 
     id("com.gorylenko.gradle-git-properties") version "1.4.21"
 }
